f530a95f056be2053464c528d9071041a6fc124c
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-24 18:37:09 +0200

    Host: nixos, Generation: 16, NixOS version: 24.05.20240606.9b5328b, Kernel: 6.6.32

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -4,15 +4,22 @@
   ...
 }: {
   home.packages = with pkgs; [
-    gnomeExtensions.dash-to-panel
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.user-themes
+    whitesur-gtk-theme
+    whitesur-icon-theme
+    whitesur-cursors
   ];
 
   dconf.settings = {
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [
-        "dash-to-panel"
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
       ];
+    };
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "whitesur-dark";
     };
   };
 }
