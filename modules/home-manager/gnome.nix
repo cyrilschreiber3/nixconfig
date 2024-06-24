b939880808bae8998b0250a74ea5fef0f0f79eb2
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-24 23:51:52 +0200

    Host: nixos, Generation: 52, NixOS version: 24.05.20240606.9b5328b, Kernel: 6.6.32

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -64,5 +64,10 @@
     "org/gnome/desktop/wm/preferences" = {
       "button-layout" = ":minimize,maximize,close";
     };
+    # "org/gnome/desktop/background" = {
+    #   picture-uri = "file://${config.home}/.background-image.jpg";
+    #   picture-uri-dark = "file://${config.home}/.background-image.jpg";
+    #   picture-options = "zoom";
+    # };
   };
 }
