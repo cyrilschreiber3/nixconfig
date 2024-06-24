43565b49c59818591855a562c77c7c58a15ed554
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-24 23:34:14 +0200

    Host: nixos, Generation: 52, NixOS version: 24.05.20240606.9b5328b, Kernel: 6.6.32

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -49,6 +49,8 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.wallpaper.combineScreens = true;
+  services.xserver.desktopManager.wallpaper.mode = "fill";
   programs.dconf.enable = true;
 
   # Configure keymap in X11
