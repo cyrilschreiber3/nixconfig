0b6ad846f62b2f6b822e9f3d3bb24417323adc6c
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-26 18:58:08 +0200

    Host: nixos, Generation: 85, NixOS version: 24.05.20240623.e4509b3, Kernel: 6.6.35

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -20,25 +20,25 @@
     })
   ];
 
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Tokyonight-Dark-BL-LB";
-  #     package = "${import ./../themes/tokyonight-gtk-theme.nix {inherit pkgs;}}";
-  #   };
-  #   cursorTheme = {
-  #     name = "WhiteSur-cursors";
-  #     package = pkgs.whitesur-cursors;
-  #   };
-  #   iconTheme = {
-  #     name = "WhiteSur";
-  #   };
-  # };
-  # xdg.configFile = {
-  #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-  #   "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-  #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  # };
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Tokyonight-Dark-BL-LB";
+      package = "${import ./../themes/tokyonight-gtk-theme.nix {inherit pkgs;}}";
+    };
+    cursorTheme = {
+      name = "WhiteSur-cursors";
+      package = pkgs.whitesur-cursors;
+    };
+    iconTheme = {
+      name = "WhiteSur";
+    };
+  };
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 
   dconf.settings = {
     "org/gnome/shell" = {
