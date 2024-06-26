1cd15bd4d5978041af37448cd4a7655b852b7cf3
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-26 18:12:24 +0200

    Host: nixos, Generation: 81, NixOS version: 24.05.20240623.e4509b3, Kernel: 6.6.35

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -36,9 +36,10 @@
   };
   xdg.configFile = {
     "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    # "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
     # "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-    "gtk-4.0/gtk-dark.css".source = ./../assets/gtk4.css;
+    "gtk-4.0/gtk.css".source = ./../assets/gtk4.css;
+    "gtk-4.0/gtk-dark.css".source = ./../assets/gtk4-d.css;
   };
 
   dconf.settings = {
