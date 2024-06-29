0cf26a8dfc4f29fac86159df73ebedc8662aa593
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-06-30 01:26:39 +0200

    Host: nixos, Generation: 123, NixOS version: 24.11.20240627.b2852eb, Kernel: 6.6.36

--- modules/home-manager/firefox_bookmarks.nix
+++ modules/home-manager/firefox_bookmarks.nix
@@ -1,184 +1,190 @@
 {
   bookmarks = [
     {
-      name = "Dashboard";
-      url = "https://dashboard.schreibernet.dev";
-    }
-    {
-      name = "iCloud";
-      url = "https://www.icloud.com/";
-    }
-    {
-      name = "Youtube";
-      url = "https://youtube.com/";
-    }
-    {
-      name = "Floatplane";
-      url = "https://floatplane.com/";
-    }
-    {
-      name = "F1 TV";
-      url = "https://f1tv.formula1.com/";
-    }
-    {
-      name = "Reddit";
-      url = "https://reddit.com/";
-    }
-    {
-      name = "Star Citizen";
+      name = "bookmarks bar";
+      toolbar = true;
       bookmarks = [
         {
-          name = "Robert Space Industries";
-          url = "https://robertsspaceindustries.com/";
+          name = "Dashboard";
+          url = "https://dashboard.schreibernet.dev";
         }
         {
-          name = "Spectrum";
-          url = "https://robertsspaceindustries.com/spectrum/";
+          name = "iCloud";
+          url = "https://www.icloud.com/";
         }
         {
-          name = "DPSCalculator";
-          url = "https://www.erkul.games/";
+          name = "Youtube";
+          url = "https://youtube.com/";
         }
         {
-          name = "Star Citizen Wiki";
-          url = "https://starcitizen.tools/";
+          name = "Floatplane";
+          url = "https://floatplane.com/";
         }
         {
-          name = "SC Trade Tools";
-          url = "https://sc-trade.tools/home";
+          name = "F1 TV";
+          url = "https://f1tv.formula1.com/";
         }
         {
-          name = "CStone: Universal Item Finder";
-          url = "https://finder.cstone.space/";
+          name = "Reddit";
+          url = "https://reddit.com/";
         }
         {
-          name = "Annuaire - Protectorat - Spectrum";
-          url = "https://robertsspaceindustries.com/spectrum/community/PROTECTORA/forum/64721/thread/les-infos-que-vous-cherchez-sont-ici-la-7-ime-va-v";
-        }
-        {
-          name = "Blueprints - Star Citizen Stuff";
-          url = "https://imgur.com/a/xKlyv#szP5XjX";
-        }
-        {
-          name = "UEX Corp. - Calculator";
-          url = "https://uexcorp.space/";
-        }
-        {
-          name = "TradeIn.Space - Route Finder";
-          url = "https://tradein.space/";
-        }
-        {
-          name = "Galactic Logistics";
-          url = "https://www.gallog.co/";
-        }
-        {
-          name = "ArtStation - Cloud Imperium Games";
-          url = "https://www.artstation.com/search?sort_by=relevance&query=Cloud%20Imperium%20Games";
-        }
-        {
-          name = "Verse Guide";
-          url = "https://verseguide.com/";
-        }
-        {
-          name = "SC Will It Fit?";
-          url = "https://hachetag.eu/sc-will-it-fit/";
-        }
-      ];
-    }
-    {
-      name = "Network admin";
-      bookmarks = [
-        {
-          name = "Media Server";
+          name = "Star Citizen";
           bookmarks = [
             {
-              name = "Local";
+              name = "Robert Space Industries";
+              url = "https://robertsspaceindustries.com/";
+            }
+            {
+              name = "Spectrum";
+              url = "https://robertsspaceindustries.com/spectrum/";
+            }
+            {
+              name = "DPSCalculator";
+              url = "https://www.erkul.games/";
+            }
+            {
+              name = "Star Citizen Wiki";
+              url = "https://starcitizen.tools/";
+            }
+            {
+              name = "SC Trade Tools";
+              url = "https://sc-trade.tools/home";
+            }
+            {
+              name = "CStone: Universal Item Finder";
+              url = "https://finder.cstone.space/";
+            }
+            {
+              name = "Annuaire - Protectorat - Spectrum";
+              url = "https://robertsspaceindustries.com/spectrum/community/PROTECTORA/forum/64721/thread/les-infos-que-vous-cherchez-sont-ici-la-7-ime-va-v";
+            }
+            {
+              name = "Blueprints - Star Citizen Stuff";
+              url = "https://imgur.com/a/xKlyv#szP5XjX";
+            }
+            {
+              name = "UEX Corp. - Calculator";
+              url = "https://uexcorp.space/";
+            }
+            {
+              name = "TradeIn.Space - Route Finder";
+              url = "https://tradein.space/";
+            }
+            {
+              name = "Galactic Logistics";
+              url = "https://www.gallog.co/";
+            }
+            {
+              name = "ArtStation - Cloud Imperium Games";
+              url = "https://www.artstation.com/search?sort_by=relevance&query=Cloud%20Imperium%20Games";
+            }
+            {
+              name = "Verse Guide";
+              url = "https://verseguide.com/";
+            }
+            {
+              name = "SC Will It Fit?";
+              url = "https://hachetag.eu/sc-will-it-fit/";
+            }
+          ];
+        }
+        {
+          name = "Network admin";
+          bookmarks = [
+            {
+              name = "Media Server";
               bookmarks = [
                 {
+                  name = "Local";
+                  bookmarks = [
+                    {
+                      name = "Plex";
+                      url = "http://plex.schreibernet.dev/";
+                    }
+                    {
+                      name = "Radarr";
+                      url = "http://radarr.schreibernet.dev/";
+                    }
+                    {
+                      name = "Sonarr";
+                      url = "http://sonarr.schreibernet.dev/";
+                    }
+                    {
+                      name = "NZBGet";
+                      url = "http://nzbget.schreibernet.dev/";
+                    }
+                  ];
+                }
+                {
                   name = "Plex";
-                  url = "http://plex.schreibernet.dev/";
+                  url = "https://plex.cyrilschreiber.ch/";
                 }
                 {
                   name = "Radarr";
-                  url = "http://radarr.schreibernet.dev/";
+                  url = "https://radarr.the127001.ch/";
                 }
                 {
                   name = "Sonarr";
-                  url = "http://sonarr.schreibernet.dev/";
+                  url = "https://sonarr.the127001.ch/";
                 }
                 {
                   name = "NZBGet";
-                  url = "http://nzbget.schreibernet.dev/";
+                  url = "https://nzbget.the127001.ch/";
+                }
+                {
+                  name = "Overseerr";
+                  url = "https://overseerr.cyrilschreiber.ch/";
+                }
+                {
+                  name = "Wizarr";
+                  url = "https://join.plex.cyrilschreiber,ch/";
                 }
               ];
             }
             {
-              name = "Plex";
-              url = "https://plex.cyrilschreiber.ch/";
+              name = "Monitoring";
+              bookmarks = [
+                {
+                  name = "Grafana";
+                  url = "http://grafana.schreibernet.dev/";
+                }
+                {
+                  name = "Prometheus";
+                  url = "http://prometheus.schreibernet.dev/";
+                }
+                {
+                  name = "InfluxDB";
+                  url = "http://influxdb.schreibernet.dev/";
+                }
+              ];
             }
             {
-              name = "Radarr";
-              url = "https://radarr.the127001.ch/";
+              name = "Azure";
+              bookmarks = [
+                {
+                  name = "Microsoft 365 - Home";
+                  url = "https://www.microsoft365.com/";
+                }
+                {
+                  name = "Microsoft 365 - Admin Center";
+                  url = "https://admin.microsoft.com/?auth_upn=CyrilSchreiber%40Schreibernet07.onmicrosoft.com&source=applauncher#/homepage";
+                }
+                {
+                  name = "OneDrive";
+                  url = "https://schreibernet07-my.sharepoint.com/personal/cyrilschreiber_schreibernet07_onmicrosoft_com/_layouts/15/onedrive.aspx?view=0";
+                }
+              ];
             }
             {
-              name = "Sonarr";
-              url = "https://sonarr.the127001.ch/";
+              name = "Internet-Box 2";
+              url = "http://192.168.1.1/";
             }
             {
-              name = "NZBGet";
-              url = "https://nzbget.the127001.ch/";
-            }
-            {
-              name = "Overseerr";
-              url = "https://overseerr.cyrilschreiber.ch/";
-            }
-            {
-              name = "Wizarr";
-              url = "https://join.plex.cyrilschreiber,ch/";
+              name = "LeGrosDede - Unraid";
+              url = "http://legrosdede.tailcd198.ts.net/";
             }
           ];
-        }
-        {
-          name = "Monitoring";
-          bookmarks = [
-            {
-              name = "Grafana";
-              url = "http://grafana.schreibernet.dev/";
-            }
-            {
-              name = "Prometheus";
-              url = "http://prometheus.schreibernet.dev/";
-            }
-            {
-              name = "InfluxDB";
-              url = "http://influxdb.schreibernet.dev/";
-            }
-          ];
-        }
-        {
-          name = "Azure";
-          bookmarks = [
-            {
-              name = "Microsoft 365 - Home";
-              url = "https://www.microsoft365.com/";
-            }
-            {
-              name = "Microsoft 365 - Admin Center";
-              url = "https://admin.microsoft.com/?auth_upn=CyrilSchreiber%40Schreibernet07.onmicrosoft.com&source=applauncher#/homepage";
-            }
-            {
-              name = "OneDrive";
-              url = "https://schreibernet07-my.sharepoint.com/personal/cyrilschreiber_schreibernet07_onmicrosoft_com/_layouts/15/onedrive.aspx?view=0";
-            }
-          ];
-        }
-        {
-          name = "Internet-Box 2";
-          url = "http://192.168.1.1/";
-        }
-        {
-          name = "LeGrosDede - Unraid";
-          url = "http://legrosdede.tailcd198.ts.net/";
         }
         {
           name = "Get active Github forks";
