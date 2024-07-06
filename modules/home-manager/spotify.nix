4a9fc123f895e9b70c91d9c119071f9c8aada8d5
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-07-06 18:07:24 +0200

    Host: nixos, Generation: 156, NixOS version: 24.11.20240701.00d80d1, Kernel: 6.6.36

--- modules/home-manager/spotify.nix
+++ modules/home-manager/spotify.nix
@@ -1,31 +1,30 @@
-{ inputs
-, pkgs
-, ...
+{
+  inputs,
+  pkgs,
+  ...
 }: {
-  imports = [ inputs.spicetify-nix.homeManagerModule ];
-  programs.spicetify =
-    let
-      tokyonightTheme = pkgs.fetchgit {
-        url = "https://github.com/Gspr-bit/Spotify-Tokyo-Night-Theme";
-        rev = "d88ca06eaeeb424d19e0d6f7f8e614e4bce962be";
-        sha256 = "sha256-cLj9v8qtHsdV9FfzV2Qf4pWO8AOBXu51U/lUMvdEXAk=";
-      };
-    in
-    {
-      enable = true;
-      windowManagerPatch = true;
-      spotifyPackage = pkgs.spotify;
-      theme = {
-        name = "TokyoNight";
-        src = tokyonightTheme;
-        appendName = false;
-        injectCss = true;
-        replaceColors = true;
-        overwriteAssets = true;
-        sidebarConfig = true;
-      };
-      colorScheme = "Night";
+  imports = [inputs.spicetify-nix.homeManagerModule];
+  programs.spicetify = let
+    tokyonightTheme = pkgs.fetchgit {
+      url = "https://github.com/Gspr-bit/Spotify-Tokyo-Night-Theme";
+      rev = "d88ca06eaeeb424d19e0d6f7f8e614e4bce962be";
+      sha256 = "sha256-cLj9v8qtHsdV9FfzV2Qf4pWO8AOBXu51U/lUMvdEXAk=";
     };
+  in {
+    enable = true;
+    windowManagerPatch = true;
+    spotifyPackage = pkgs.spotify;
+    theme = {
+      name = "Tokyo";
+      src = tokyonightTheme;
+      appendName = false;
+      injectCss = true;
+      replaceColors = true;
+      overwriteAssets = true;
+      sidebarConfig = true;
+    };
+    colorScheme = "Night";
+  };
 
   # # Force Spotify to use X11 backend
   # xdg.desktopEntries.spotify = {
