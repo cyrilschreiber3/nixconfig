a259dead96c18e568e6101f94aa48ca3db8213e1
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-03 21:21:58 +0200

    Host: nixos, Generation: 215, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -17,36 +17,36 @@
   home.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # enable vscode-server
-  imports = [
-    "${
-      fetchTarball {
-        url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
-        sha256 = "1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
-      }
-    }/modules/vscode-server/home.nix"
-  ];
-  services.vscode-server.enable = true;
-  services.vscode-server.enableFHS = true;
-  services.vscode-server.installPath = "$HOME/.vscode";
-  services.vscode-server.extraRuntimeDependencies = with pkgs; [
-    nixd
-    alejandra
-    nixpkgs-fmt
-  ];
+  # imports = [
+  #   "${
+  #     fetchTarball {
+  #       url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
+  #       sha256 = "1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
+  #     }
+  #   }/modules/vscode-server/home.nix"
+  # ];
+  # services.vscode-server.enable = true;
+  # services.vscode-server.enableFHS = true;
+  # services.vscode-server.installPath = "$HOME/.vscode";
+  # services.vscode-server.extraRuntimeDependencies = with pkgs; [
+  #   nixd
+  #   alejandra
+  #   nixpkgs-fmt
+  # ];
 
-  # systemd.user.services.vscode-tunnel = {
-  #   Unit = {
-  #     Description = "Visual Studio Code Tunnel";
-  #     After = ["network.target"];
-  #   };
-  #   Service = {
-  #     Type = "simple";
-  #     ExecStart = "${pkgs.vscode}/lib/vscode/bin/code-tunnel '--verbose' '--cli-data-dir' '${config.home.homeDirectory}/.vscode/cli' 'tunnel' 'service' 'internal-run'";
-  #     Restart = "always";
-  #     RestartSec = 10;
-  #   };
-  #   Install = {
-  #     WantedBy = ["default.target"];
-  #   };
-  # };
+  systemd.user.services.code-tunnel = {
+    Unit = {
+      Description = "Visual Studio Code Tunnel";
+      After = ["network.target"];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.vscode}/lib/vscode/bin/code-tunnel --verbose --cli-data-dir${config.home.homeDirectory}/.vscode/cli tunnel service internal-run";
+      Restart = "always";
+      RestartSec = 10;
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+  };
 }
