14c1ca818dffdae9fee5e4f34465e4d4be5aa120
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-03 13:37:20 +0200

    Host: nixos, Generation: 207, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhsWithPackages (ps:
@@ -22,4 +26,20 @@
     }/modules/vscode-server/home.nix"
   ];
   services.vscode-server.enable = true;
+
+  systemd.user.services.vscode-tunnel = {
+    Unit = {
+      Description = "Visual Studio Code Tunnel";
+      After = ["network.target"];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.vscode}/lib/vscode/bin/code-tunnel '--verbose' '--cli-data-dir' '${config.home.homeDirectory}/.vscode/cli' 'tunnel' 'service' 'internal-run'";
+      Restart = "always";
+      RestartSec = 10;
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+  };
 }
