5e2895d1bc876ff3b9a199e65e7e914417e07bf1
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-04 00:12:27 +0200

    Host: nixos, Generation: 230, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -4,7 +4,7 @@
     pls
     # thefuck
     oh-my-zsh
-    (nerdfonts.override {fonts = ["Meslo" "JetBrainsMono"];})
+    nerdfonts
     chroma # required by the colorize plugin for omz
     zsh-autosuggestions
     zsh-nix-shell
