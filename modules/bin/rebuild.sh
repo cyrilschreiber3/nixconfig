e16fcb46a44f94562e8b7447d8d4d079ac10347a
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-04 02:24:12 +0200

    Host: nixos, Generation: 253, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- modules/bin/rebuild.sh
+++ modules/bin/rebuild.sh
@@ -5,10 +5,12 @@
 # get parameters
 force=false
 update=false
+garbageCollect=false
 while getopts "fu" flag; do
     case $flag in
     f) force=true ;;
     u) update=true ;;
+    g) garbageCollect=true ;;
     esac
 done
 
@@ -111,7 +113,7 @@ echo "$push_output" | tail -n 3
 # Delete older generations
 currentGenCount=$(nixos-rebuild list-generations | wc -l)
 maxGenCount=20
-if [ "$currentGenCount" -gt 25 ]; then
+if [ "$currentGenCount" -gt 25 || $garbageCollect -eq true ]; then
     printf "Deleting old generations..."
     (nix-env --delete-generations +$maxGenCount >>nixos-gc.log 2>&1 && sudo nix-env --delete-generations +$maxGenCount -p /nix/var/nix/profiles/system >>nixos-gc.log 2>&1) &
     pid=$! && spinner $pid && wait $pid
