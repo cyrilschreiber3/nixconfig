a14321d8e510361d8bba01cb6e8ac79a20e5e757
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-04 15:41:21 +0200

    Host: nixos, Generation: 258, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -57,26 +57,26 @@
           };
           file = "zsh-interactive-cd.plugin.zsh";
         }
-        # {
-        #   name = "powerlevel10k-config";
-        #   src = ./../dotfiles/p10k;
-        #   file = "p10k.zsh";
-        # }
-        # {
-        #   name = "zsh-powerlevel10k";
-        #   src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-        #   file = "powerlevel10k.zsh-theme";
-        # }
+        {
+          name = "powerlevel10k-config";
+          src = ./../dotfiles/p10k;
+          file = "p10k.zsh";
+        }
+        {
+          name = "zsh-powerlevel10k";
+          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+          file = "powerlevel10k.zsh-theme";
+        }
       ];
 
-      # initExtraFirst = ''
-      #   # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      #   # Initialization code that may require console input (password prompts, [y/n]
-      #   # confirmations, etc.) must go above this block; everything else may go below.
-      #   if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-      #     source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      #   fi
-      # '';
+      initExtraFirst = ''
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '';
 
       initExtra = ''
         # zsh-interactive-cd plugin
