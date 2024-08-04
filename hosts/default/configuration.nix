bb029403795d94670fa2b20b6901142b71565718
cyrilschreiber3 <contact@cyrilschreiber.ch>
2024-08-04 02:32:42 +0200

    Host: nixos, Generation: 256, NixOS version: 24.11.20240731.9f918d6, Kernel: 6.6.43

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -17,7 +17,6 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
-  boot.loader.grub.configurationLimit = 10;
 
   networking.hostName = "nixos";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
