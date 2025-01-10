150b2ab9f773bcb7705a764771134feab4098a04
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-01-11 00:35:45 +0100

    Host: scorpius-cl-01, Generation: 43, NixOS version: 25.05.20250104.8f3e1f8, Kernel: 6.6.69

--- modules/nixos/homelab-shares.nix
+++ modules/nixos/homelab-shares.nix
@@ -1,31 +1,35 @@
-{...}: {
+{pkgs, ...}: {
   boot.kernelModules = ["autofs"];
 
   services.autofs = {
     enable = true;
-    autoMaster = ''
-      /- /etc/auto.direct --timeout 3600
+    autoMaster = let
+      mapConf = pkgs.writeText "auto" ''
+        /mnt /etc/auto.direct --timeout 3600
+      '';
+    in ''
+      /auto file:${mapConf}
     '';
   };
 
   environment.etc = {
-    "autofs.conf" = {
-      text = ''
-        [ autofs ]
-        timeout=300
-        browse_mode=no
-        mount_nfs_default_protocol=4
-        [ amd ]
-        dismount_interval=300
-      '';
-      mode = "0644";
-    };
+    # "autofs.conf" = {
+    #   text = ''
+    #     [ autofs ]
+    #     timeout=300
+    #     browse_mode=no
+    #     mount_nfs_default_protocol=4
+    #     [ amd ]
+    #     dismount_interval=300
+    #   '';
+    #   mode = "0644";
+    # };
     "auto.direct" = {
       text = ''
-        /mnt/Media1 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media1/Media1
-        /mnt/Media2 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media2/Media2
-        /mnt/Vault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Vault/VaultData
-        /mnt/TurboVault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/TurboVault/TurboVaultData
+        Media1 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media1/Media1
+        Media2 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media2/Media2
+        Vault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Vault/VaultData
+        TurboVault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/TurboVault/TurboVaultData
       '';
       mode = "0644";
     };
