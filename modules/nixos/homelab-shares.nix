6c184a38c7c11aab1451159832f9d15656453292
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-01-10 17:44:41 +0100

    Host: scorpius-cl-01, Generation: 37, NixOS version: 25.05.20250104.8f3e1f8, Kernel: 6.6.69

--- modules/nixos/homelab-shares.nix
+++ modules/nixos/homelab-shares.nix
@@ -6,6 +6,7 @@
       /mnt/Media2 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media2/Media2
       /mnt/Vault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Vault/VaultData
       /mnt/TurboVault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/TurboVault/TurboVaultData
+
     '';
   };
 }
