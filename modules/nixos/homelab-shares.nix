08420b4e5eaf0b00551629f00962ae96231eb363
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-01-10 18:16:27 +0100

    Host: scorpius-cl-01, Generation: 38, NixOS version: 25.05.20250104.8f3e1f8, Kernel: 6.6.69

--- modules/nixos/homelab-shares.nix
+++ modules/nixos/homelab-shares.nix
@@ -2,11 +2,20 @@
   services.autofs = {
     enable = true;
     autoMaster = ''
-      /mnt/Media1 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media1/Media1
-      /mnt/Media2 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media2/Media2
-      /mnt/Vault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Vault/VaultData
-      /mnt/TurboVault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/TurboVault/TurboVaultData
+      /net /etc/auto.net --timeout 3600
 
     '';
+  };
+
+  environment.etc = {
+    "auto.net" = {
+      text = ''
+        Media1 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media1/Media1
+        Media2 -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Media2/Media2
+        Vault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/Vault/VaultData
+        TurboVault -fstype=nfs,rw,nfsvers=4 192.168.1.10:/mnt/TurboVault/TurboVaultData
+      '';
+      mode = "0644";
+    };
   };
 }
