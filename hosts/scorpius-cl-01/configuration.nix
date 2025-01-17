05b55dfa9ae64394e2385a7443b7201e88005eef
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-01-17 22:59:52 +0100

    Host: scorpius-cl-01, Generation: 68, NixOS version: 25.05.20250104.8f3e1f8, Kernel: 6.6.69

--- hosts/scorpius-cl-01/configuration.nix
+++ hosts/scorpius-cl-01/configuration.nix
@@ -160,6 +160,11 @@
     customShares = [];
   };
 
+  x11vncConfig = {
+    enable = true;
+    mainUser = "cyril";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
