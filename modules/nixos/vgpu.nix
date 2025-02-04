16c4c1aa1e214a39825f40e2492785a943ecf710
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-02-04 22:26:39 +0100

    Host: scorpius-cl-01, Generation: 101, NixOS version: 25.05.20250201.3a22805, Kernel: 6.6.74

--- modules/nixos/vgpu.nix
+++ modules/nixos/vgpu.nix
@@ -54,7 +54,7 @@ in {
       debug = true;
       listen.ip = "0.0.0.0";
     };
-    networking.firewall.allowedTCPPorts = [443];
+    networking.firewall.interfaces.virbr0.allowedTCPPorts = [443];
 
     boot.kernelParams = [
       "${cfg.cpuType}_iommu=on"
