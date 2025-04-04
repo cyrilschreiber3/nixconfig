d4d061454d9d5e30134821ba6b0c385aa9463610
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-04-04 17:28:09 +0200

    Host: scorpius-cl-01, Generation: 127, NixOS version: 25.05.20250318.b6eaf97, Kernel: 6.6.83

--- modules/specialisations/default.nix
+++ modules/specialisations/default.nix
@@ -1,12 +1,23 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   imports = [
     ./gaming.nix
   ];
   environment.sessionVariables = lib.mkIf (config.specialisation != {}) {
-    SPECIALISATION = "NONE";
+    SPECIALISATION = "none";
   };
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "switch-spec" ''
+      if [ $# -ne 1 ]; then
+        echo "Usage: switch-spec <specialisation>"
+        exit 1
+      fi
+
+      sudo /nix/var/nix/profiles/system/specialisation/$1/bin/switch-to-configuration switch
+    '')
+  ];
 }
--- modules/specialisations/gaming.nix
+++ modules/specialisations/gaming.nix
@@ -1,4 +1,31 @@
-{...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   specialisation."gaming".configuration = {
+    environment.sessionVariables = lib.mkDefault {
+      SPECIALISATION = "gaming";
+    };
+    system.nixos.tags = lib.mkDefault [
+      "gaming"
+    ];
+
+    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    boot.kernelParams = [
+      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+      "nvidia-drm.modeset=1"
+    ];
+    hardware = {
+      nvidia = {
+        open = false;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+      };
+      graphics = {
+        enable = true;
+        # enable32bit = true;
+      };
+    };
   };
 }
