e58f5230ddbdf02c0af32342167649506b8753a2
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-04-28 23:37:00 +0200

    Host: scorpius-cl-01, Generation: 164, NixOS version: 25.05.20250417.b024ced, Kernel: 6.6.87

--- hosts/scorpius-cl-01/configuration.nix
+++ hosts/scorpius-cl-01/configuration.nix
@@ -24,29 +24,29 @@
   ];
 
   # Bootloader
-  # boot.loader = {
-  #   efi = {
-  #     # canTouchEfiVariables = true;
-  #     efiSysMountPoint = "/boot/efi";
-  #   };
-  #   grub = {
-  #     enable = true;
-  #     efiSupport = true;
-  #     efiInstallAsRemovable = true;
-  #     #  device = "/dev/sda"; # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-  #     useOSProber = true;
-  #   };
-  # };
+  boot.loader = {
+    efi = {
+      # canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      #  device = "/dev/sda"; # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+      useOSProber = true;
+    };
+  };
 
-  boot.loader.grub.enable = true;
+  # boot.loader.grub.enable = true;
 
-  # boot.loader.grub.device = "/dev/sda"; # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+  # # boot.loader.grub.device = "/dev/sda"; # no need to set devices, disko will add all devices that have a EF02 partition to the list already
 
-  boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiSupport = true;
 
-  boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
 
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.useOSProber = true;
 
   boot.binfmt.emulatedSystems = [
     "aarch64-linux"
