695861d8157c14913f3bf5e8c8c288b53086d097
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-05-14 23:36:28 +0200

    Host: scorpius-cl-01, Generation: 200, NixOS version: 25.05.20250508.dda3dcd, Kernel: 6.6.89

--- hosts/scorpius-cl-01/configuration.nix
+++ hosts/scorpius-cl-01/configuration.nix
@@ -127,8 +127,7 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    # pulse.enable = true;
-    jack.enable = true;
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
