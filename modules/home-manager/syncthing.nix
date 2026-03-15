be9b9a702f69b9ae7176e0e8c1db449a34a35379
cyrilschreiber3 <contact@cyrilschreiber.ch>
2026-03-15 17:57:58 +0100

    Host: scorpius-cl-01, Generation: 301, NixOS version: 25.11.20251108.b6a8526, Kernel: 6.6.116

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -63,7 +63,13 @@ in {
             id = "admin";
             label = "admin";
             path = "~/Documents/admin";
-            devices = ["scorpius-cl-01" "macbook" "mercury-fs-01"];
+            devices = ["scorpius-cl-01" "mercury-fs-01"];
+          };
+          pompiers = {
+            id = "pompiers";
+            label = "pompiers";
+            path = "~/Documents/pompiers";
+            devices = ["scorpius-cl-01" "mercury-fs-01"];
           };
         };
       };
